80b606a09e720e19318a0f4578fde2f9393f4ff4
Anugra Shah <agshah@uwaterloo.ca>
2022-04-08 23:32:31 -0400

    ACTUAL_INIT

--- api/app.py
+++ api/app.py
@@ -1,8 +1,30 @@
 import time
-from flask import Flask
+from flask import Flask, request
+from guess import random_country
+from guess import take_a_guess
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+destination = ""
+
+@app.route('/random_country/<code>')
+def get_random_country(code):
+    print(code)
+    destination = code
+    return {'random_country': random_country()}
+
+@app.route('/make_guess/<country>/<destination>')
+def make_guess(country, destination):
+    print(country)
+    print(destination)
+    ret_arr = take_a_guess(country, destination)
+    ret_obj = {'response': {
+            'distance': ret_arr[0],
+            'direction': ret_arr[1]
+        }
+    }
+    return ret_obj
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,131 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect, useCallback } from "react";
+import ReactDOM from "react-dom";
+import Select from 'react-select'
+import {names} from "./country_names";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function get_random_country() {
+    var random_country = names[Math.floor(Math.random()*names.length)];
+    return random_country;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const country_names = names;
+
+const arr_of_guesses = [];
+
+const make_guess_str = "/make_guess/";
+const random_country_url = "/random_country/";
+
+const random_country = get_random_country();
+
+function RandomCountry() {
+    const [randomCountry, setRandomCountry] = useState(random_country.value);
+
+    var fetch_url = random_country_url.concat(randomCountry);
+    
+    useEffect(() => {
+
+        fetch(fetch_url).then(res => res.json()).then(data => {
+            // console.log(data.random_country);
+        })});
+
+    return <WebPage />
+
+};
+
+
+function WebPage() {
+    const name = "Anugra Shah";
+    const [country, setCountry] = useState({
+        value: "",
+        label: ""
+    });
+    const [destination, setDestination] = useState(random_country.value);
+    const [guess_list, setGuesses] = useState(arr_of_guesses);
+    const [guesses_number, setGuessesNumber] = useState(1)
+    const [guessResponse, setGuessResponse] = useState({});
+
+    
+
+    var fetch_country;
+    // make variable that fetches distance from flask api
+
+    // useEffect(() => {
+    //     fetch(fetch_country).then(res => res.json()).then(data => {
+    //       setGuessResponse(data.response);
+    //     });
+    // }, []);
+
+
+    const fetchData = useCallback(
+        () => {
+            fetch(fetch_country).then(res => res.json()).then(data => {
+                setGuessResponse(data.response);
+                setGuessesNumber(guesses_number + 1);
+                setGuesses(guess_list.concat(
+                    {
+                        guess_no: guesses_number,
+                        guessed_country: country.value,
+                        distance: data.response.distance,
+                        bearing: data.response.direction,
+                        destination: destination
+                    }
+                ))
+            })
+        },
+        [fetch_country = make_guess_str.concat(country.value).concat("/").concat(destination)],
+    );
+
+    return <>
+        <section>
+            <header>
+                <h1>Hello {name}, Welcome to Worldle </h1>
+            </header>
+            {/* <h2>DESTINATION: {destination}</h2> */}
+        </section>
+        <h2>Guess Country:</h2>
+
+        {guessResponse.distance === 0 &&
+            <p>CONGRATULATIONS! YOU GOT IT.</p>
+        }
+
+        {((guesses_number === 6) & (guessResponse.distance !== 0)) &&
+            <p> SORRY YOU'RE OUT OF GUESSES. YOU'RE TRASH. THE ANSWER WAS {destination}</p>
+        }
+
+
+
+        {((guesses_number < 6) & (guessResponse.distance !== 0)) &&
+                <><Select
+                options={country_names}
+                onChange={setCountry} /><p>You Selected: {country.value}</p>
+                <button onClick={fetchData}>
+                    Confirm Guess
+                </button>
+                </>
+        }
+
+        {guessResponse !== {} &&
+                    <>
+                    <table>
+                        <tr>
+                            <th>Guess Number</th>
+                            <th>Guessed Country</th>
+                            <th>Distance to Destination</th>
+                            <th>Direction to Destination</th>
+                        </tr>
+                        {guess_list.map(d => (
+                            <tr>
+                                <td>{d.guess_no}</td>
+                                <td>{d.guessed_country}</td>
+                                <td>{d.distance} km</td>
+                                <td>{d.bearing}</td>
+                            </tr>))}
+                    </table>
+                    </>
+        }
+  
+    </>
+}
+
+
+ReactDOM.render(<RandomCountry />, document.getElementById("root"));
